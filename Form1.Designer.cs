516554ecaa2bb8cb84ab2e7fa0fbf93139c92576
zhjjssyf20120925 <875135458@qq.com>
2015-08-31 17:06:19 +0800

    Modify code.
    design the Software interface.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormAutomaticallyIntegral
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,22 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dgvList = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbWebName = new System.Windows.Forms.TextBox();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddInfo = new System.Windows.Forms.Button();
+            this.btnUpdateInfo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbObj = new System.Windows.Forms.TextBox();
+            this.colWebName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colObj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotalIntegral = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColIsSuccess = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLastTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDeleteInfo = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cbAutoAction = new System.Windows.Forms.CheckBox();
+            this.pbAction = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // dgvList
+            // 
+            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colWebName,
+            this.colObj,
+            this.colTotalIntegral,
+            this.ColIsSuccess,
+            this.colLastTime});
+            this.dgvList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvList.Location = new System.Drawing.Point(3, 21);
+            this.dgvList.Name = "dgvList";
+            this.dgvList.RowTemplate.Height = 27;
+            this.dgvList.Size = new System.Drawing.Size(553, 416);
+            this.dgvList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网站名称:";
+            // 
+            // tbWebName
+            // 
+            this.tbWebName.Location = new System.Drawing.Point(101, 26);
+            this.tbWebName.Name = "tbWebName";
+            this.tbWebName.Size = new System.Drawing.Size(238, 25);
+            this.tbWebName.TabIndex = 2;
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(101, 69);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(238, 25);
+            this.tbUrl.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "积分网址:";
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(101, 193);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.PasswordChar = '*';
+            this.tbPwd.Size = new System.Drawing.Size(238, 25);
+            this.tbPwd.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "登录密码:";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(101, 154);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(238, 25);
+            this.tbUserName.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "用户名称:";
+            // 
+            // btnAddInfo
+            // 
+            this.btnAddInfo.Location = new System.Drawing.Point(23, 239);
+            this.btnAddInfo.Name = "btnAddInfo";
+            this.btnAddInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnAddInfo.TabIndex = 9;
+            this.btnAddInfo.Text = "新增信息";
+            this.btnAddInfo.UseVisualStyleBackColor = true;
+            this.btnAddInfo.Click += new System.EventHandler(this.btnAddInfo_Click);
+            // 
+            // btnUpdateInfo
+            // 
+            this.btnUpdateInfo.Location = new System.Drawing.Point(140, 239);
+            this.btnUpdateInfo.Name = "btnUpdateInfo";
+            this.btnUpdateInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateInfo.TabIndex = 9;
+            this.btnUpdateInfo.Text = "更新信息";
+            this.btnUpdateInfo.UseVisualStyleBackColor = true;
+            this.btnUpdateInfo.Click += new System.EventHandler(this.btnUpdateInfo_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 440);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "执行明细";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.tbObj);
+            this.groupBox2.Controls.Add(this.tbPwd);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnDeleteInfo);
+            this.groupBox2.Controls.Add(this.btnUpdateInfo);
+            this.groupBox2.Controls.Add(this.btnAddInfo);
+            this.groupBox2.Controls.Add(this.tbWebName);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbUrl);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbUserName);
+            this.groupBox2.Location = new System.Drawing.Point(577, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(437, 275);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "信息管理";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "积分对象:";
+            // 
+            // tbObj
+            // 
+            this.tbObj.Location = new System.Drawing.Point(101, 114);
+            this.tbObj.Name = "tbObj";
+            this.tbObj.Size = new System.Drawing.Size(238, 25);
+            this.tbObj.TabIndex = 11;
+            // 
+            // colWebName
+            // 
+            this.colWebName.HeaderText = "网站名称";
+            this.colWebName.Name = "colWebName";
+            // 
+            // colObj
+            // 
+            this.colObj.HeaderText = "积分对象";
+            this.colObj.Name = "colObj";
+            // 
+            // colTotalIntegral
+            // 
+            this.colTotalIntegral.HeaderText = "当前积分";
+            this.colTotalIntegral.Name = "colTotalIntegral";
+            // 
+            // ColIsSuccess
+            // 
+            this.ColIsSuccess.HeaderText = "获取成功";
+            this.ColIsSuccess.Name = "ColIsSuccess";
+            // 
+            // colLastTime
+            // 
+            this.colLastTime.HeaderText = "刷新时间";
+            this.colLastTime.Name = "colLastTime";
+            // 
+            // btnDeleteInfo
+            // 
+            this.btnDeleteInfo.Location = new System.Drawing.Point(264, 239);
+            this.btnDeleteInfo.Name = "btnDeleteInfo";
+            this.btnDeleteInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteInfo.TabIndex = 9;
+            this.btnDeleteInfo.Text = "删除信息";
+            this.btnDeleteInfo.UseVisualStyleBackColor = true;
+            this.btnDeleteInfo.Click += new System.EventHandler(this.btnDeleteInfo_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pbAction);
+            this.groupBox3.Controls.Add(this.cbAutoAction);
+            this.groupBox3.Controls.Add(this.btnStart);
+            this.groupBox3.Location = new System.Drawing.Point(577, 309);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(437, 139);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "执行操作";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(23, 44);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始执行";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cbAutoAction
+            // 
+            this.cbAutoAction.AutoSize = true;
+            this.cbAutoAction.Location = new System.Drawing.Point(282, 48);
+            this.cbAutoAction.Name = "cbAutoAction";
+            this.cbAutoAction.Size = new System.Drawing.Size(149, 19);
+            this.cbAutoAction.TabIndex = 1;
+            this.cbAutoAction.Text = "软件启动自动执行";
+            this.cbAutoAction.UseVisualStyleBackColor = true;
+            this.cbAutoAction.CheckedChanged += new System.EventHandler(this.cbAutoAction_CheckedChanged);
+            // 
+            // pbAction
+            // 
+            this.pbAction.Location = new System.Drawing.Point(23, 112);
+            this.pbAction.Name = "pbAction";
+            this.pbAction.Size = new System.Drawing.Size(408, 11);
+            this.pbAction.TabIndex = 2;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1017, 519);
+            this.ClientSize = new System.Drawing.Size(1032, 460);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动获取积分";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbWebName;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddInfo;
+        private System.Windows.Forms.Button btnUpdateInfo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbObj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colWebName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colObj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotalIntegral;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColIsSuccess;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLastTime;
+        private System.Windows.Forms.Button btnDeleteInfo;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox cbAutoAction;
+        private System.Windows.Forms.ProgressBar pbAction;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,11 +9,105 @@ using System.Windows.Forms;
 
 namespace WinFormAutomaticallyIntegral
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        #region [  成员变量  ]
+
+        /// <summary>
+        /// 获取或者设置一个值  用于判断当前是否自动关闭软件
+        /// 默认自动关闭，只有在获取积分失败的情况下才会失效。
+        /// </summary>
+        private bool isCloseForm = true;
+        #endregion
+
+        #region [  私有方法  ]
+
+        #endregion
+
+        #region [  成员方法  ]
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public frmMain()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 窗体加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        /// <summary>
+        /// 新增网站信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 更新网站信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUpdateInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 删除网站信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleteInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 开始执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 标记下次软件启动自动执行积分获取
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbAutoAction_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 软件关闭之前
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (cbAutoAction.CheckState == CheckState.Checked)
+            {
+                if (MessageBox.Show("获取积分结束,是否关闭软件?", "提示!", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk) == DialogResult.Cancel)
+                {
+
+                }
+            }
+        }
+        #endregion
     }
 }
